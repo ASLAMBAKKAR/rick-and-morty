3f371236d79eeb4d8c5d035f93263abe6a8e1423
Aslam Buks <aslambakkar22@gmail.com>
2026-05-20 22:42:00 +0530

    pagination

--- script.js
+++ script.js
@@ -6,47 +6,52 @@ cards.classList.add("card-container");
 container.appendChild(header);
 header.classList.add("header");
 header.textContent = "RiCk aNd mOrtY";
-
 container.appendChild(cards);
 
+let currentPage = 1;
+
 const getData = async function () {
-  const characters = await fetch(
-    "https://rickandmortyapi.com/api/character?limit=100",
-  ).then((res) => {
-    return res.json();
-  });
+  cards.innerHTML = "";
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${currentPage}`,
+    );
+    const data = await response.json();
 
-  console.log({ characters });
+     const limitedResults = data.results.slice(0, 10);
 
-  displayCharacters(characters);
+    displayCharacters(limitedResults);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 function displayCharacters(characters) {
-  characters.results.forEach((charecter) => {
+  characters.forEach((character) => {
     const innerDiv = document.createElement("div");
     const nameHeading = document.createElement("h3");
     const image = document.createElement("img");
     const gender = document.createElement("p");
-    const name = charecter.name;
-    const id = charecter.id;
-    const button = document.createElement("button");
-    button.classList.add("viewmore", `charecter-${id}`);
     const species = document.createElement("p");
     const location = document.createElement("p");
+    const button = document.createElement("button");
 
+    const id = character.id;
     innerDiv.classList.add("innerDiv");
 
-    image.src = charecter.image;
-    [gender, species, location].forEach((item) =>
-      item.classList.add("elements", `charecter-${id}`),
-    );
-
-    gender.textContent = " Gender: " + charecter.gender;
-    location.textContent = " Location: " + charecter.location.name;
-    species.textContent = " Species: " + charecter.species;
+    image.src = character.image;
+    button.classList.add("viewmore", `char-${id}`);
     button.textContent = "View More";
 
-    nameHeading.textContent = name;
+    [gender, species, location].forEach((item) => {
+      item.classList.add("elements", `char-${id}`);
+      item.style.display = "none"; 
+    });
+
+    gender.textContent = " Gender: " + character.gender;
+    location.textContent = " Location: " + character.location.name;
+    species.textContent = " Species: " + character.species;
+    nameHeading.textContent = character.name;
 
     [image, nameHeading, gender, location, species, button].forEach((item) =>
       innerDiv.appendChild(item),
@@ -55,63 +60,57 @@ function displayCharacters(characters) {
     cards.appendChild(innerDiv);
   });
 
-  const toggleButton = function () {
-    const viewMoreButtons = document.querySelectorAll(".viewmore");
-
-    viewMoreButtons.forEach((button) => {
-      button.addEventListener("click", function (event) {
-        const elementsToDisplayClassName = event.target.classList[1];
-        const elements = document.querySelectorAll(
-          `.elements.${elementsToDisplayClassName}`,
-        );
-
-        if (elements.length > 0) {
-          elements.forEach((element) => {
-            if (element.style.display === "block") {
-              element.style.display = "none";
-              event.target.textContent = "View More";
-              return;
-            }
-            element.style.display = "block";
-            event.target.textContent = "Hide Content";
-          });
-        }
-      });
-    });
-  };
-  toggleButton();
+  setupToggleButtons();
 }
 
-let currentPage = 1;
+function setupToggleButtons() {
+  const viewMoreButtons = document.querySelectorAll(".viewmore");
+
+  viewMoreButtons.forEach((button) => {
+    button.onclick = function (event) {
+      const idClass = event.target.classList[1];
+      const elements = document.querySelectorAll(`.elements.${idClass}`);
+
+      elements.forEach((element) => {
+        if (element.style.display === "block") {
+          element.style.display = "none";
+          event.target.textContent = "View More";
+        } else {
+          element.style.display = "block";
+          event.target.textContent = "Hide Content";
+        }
+      });
+    };
+  });
+}
 
 function pagination() {
   const paginationDiv = document.createElement("div");
   paginationDiv.classList.add("navDiv");
 
-  // if(id <= 5){
-
-  // }
-
   const previousButton = document.createElement("button");
   previousButton.classList.add("previousButton");
-  previousButton.textContent = "<<<";
-
-  previousButton.addEventListener("click", () =>
-    console.log("previous button clicked"),
-  );
+  previousButton.textContent = " <<< PREVIOUS  ";
 
   const nextButton = document.createElement("button");
   nextButton.classList.add("nextButton");
-  nextButton.textContent = ">>>";
-  nextButton.addEventListener("click", () =>
-    console.log("next button clicked"),
-  );
+  nextButton.textContent = "NEXT >>> ";
+
+  previousButton.addEventListener("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      getData();
+    }
+  });
+
+  nextButton.addEventListener("click", () => {
+    currentPage++;
+    getData();
+  });
 
   paginationDiv.appendChild(previousButton);
   paginationDiv.appendChild(nextButton);
-
   document.body.appendChild(paginationDiv);
 }
-
 pagination();
 getData();
