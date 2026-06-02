b52d20f8d839888b28eb7652107da87f8b4d9170
Aslam Buks <aslambakkar22@gmail.com>
2026-06-02 23:55:08 +0530

    search results

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 const container = document.getElementById("root");
 const cards = document.createElement("div");
 const header = document.createElement("h3");
-
+const searchContainer= document.createElement("div");
+const search = document.createElement("input");
+const resultContainer = document.createElement("div");
+ 
+resultContainer.classList.add("result-container");
+search.classList.add("searchBar");
+search.placeholder = "⌕";
+searchContainer.classList.add("searchdiv")
 cards.classList.add("card-container");
 container.appendChild(header);
 header.classList.add("header");
 header.textContent = "RiCk aNd mOrtY";
+container.appendChild(searchContainer);
+searchContainer.appendChild(search);
 container.appendChild(cards);
+container.appendChild(resultContainer);
+
 
 let currentPage = 1;
+let pageIndicator; 
+
 
 const getData = async function () {
   cards.innerHTML = "";
+   if (pageIndicator) {
+    pageIndicator.textContent = ` Page ${currentPage} `;
+  }
   try {
     const response = await fetch(
       `https://rickandmortyapi.com/api/character?page=${currentPage}`,
     );
     const data = await response.json();
+    
 
      const limitedResults = data.results.slice(0, 15);
 
     displayCharacters(limitedResults);
   } catch (error) {
+
     console.error("Error fetching data:", error);
   }
 };
@@ -59,6 +77,7 @@ function displayCharacters(characters) {
 
     cards.appendChild(innerDiv);
   });
+ 
 
   setupToggleButtons();
 }
@@ -84,13 +103,65 @@ function setupToggleButtons() {
   });
 }
 
+function searCharacters(){
+
+  const input = document.querySelector('input');
+  document
+  let timeout;
+  
+
+  input.addEventListener("input", (e)=>{
+    const query = event.target.value.trim();
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(()=>{
+        fetchData(query);
+      
+    },300);
+   
+
+
+    console.log(e.target.value)
+  })
+  async function fetchData(query) {
+  cards.innerHTML = "";
+
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?name=${query}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Character not found!!!");
+    }
+    const data = await response.json();
+
+    displayCharacters(data.results);
+  } catch (error) {
+    cards.innerHTML = `
+      <h1>No characters found</h1>
+    `;
+    console.error(error);
+  }
+}
+  }
+  
+ searCharacters()
+
+
 function pagination() {
+  
   const paginationDiv = document.createElement("div");
   paginationDiv.classList.add("navDiv");
 
   const previousButton = document.createElement("button");
   previousButton.classList.add("previousButton");
   previousButton.textContent = " <<< PREVIOUS  ";
+
+  pageIndicator = document.createElement("span");
+  pageIndicator.style.margin = "0 15px";
+  pageIndicator.textContent = ` Page ${currentPage} `;
 
   const nextButton = document.createElement("button");
   nextButton.classList.add("nextButton");
@@ -99,6 +170,7 @@ function pagination() {
   previousButton.addEventListener("click", () => {
     if (currentPage > 1) {
       currentPage--;
+
       getData();
     }
   });
@@ -107,10 +179,13 @@ function pagination() {
     currentPage++;
     getData();
   });
+  
 
   paginationDiv.appendChild(previousButton);
+  paginationDiv.appendChild(pageIndicator)
   paginationDiv.appendChild(nextButton);
   document.body.appendChild(paginationDiv);
 }
 pagination();
 getData();
+ 
